4d3d37f66ed8c7c3054ea5d2523165194b496c54
Tin_Le <jackletrungtin@gmail.com>
2022-11-08 10:34:08 -0800

    added a loop for input name

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,21 @@
 // alert("HE IS COMING BACK NEXT WEEK")
 // console.log("RUN")
 
-for (let i =1; i < 1025; i = i *2) {
-    console.log(i);
-}
+// for (let i =1; i < 1025; i = i *2) {
+//     console.log(i);
+// }
 
-let i = 1
-while (i<1024){
-    i = i* 2
-    console.log(i)
-}
+// let i = 1
+// while (i<1024){
+//     i = i* 2
+//     console.log(i)
+// }
 
+let input = "";
+
+do {
+    input = prompt("What is your name?");
+}while(!isNaN(Number(input)));
+    console.log(input);
+
+alert(`Welcome  ${input}`);
