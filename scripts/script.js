ffa136edffcd7d485ecf4a4292c4d5d92d826498
Tin_Le <jackletrungtin@gmail.com>
2022-11-08 09:54:24 -0800

    added non-infinite java loop

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+// alert("HE IS COMING BACK NEXT WEEK")
+// console.log("RUN")
+
+for (let i =1; i < 1025; i = i *2) {
+    console.log(i);
+}
+
+let i = 1
+while (i<1024){
+    i = i* 2
+    console.log(i)
+}
