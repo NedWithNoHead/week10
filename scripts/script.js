62fced4e4502cd03218c805d64082a482fc0f579
Tin_Le <jackletrungtin@gmail.com>
2022-11-08 11:08:57 -0800

    added a bunch of javascript

--- scripts/script.js
+++ scripts/script.js
@@ -11,11 +11,42 @@
 //     console.log(i)
 // }
 
-let input = "";
+// let input = "";
 
-do {
-    input = prompt("What is your name?");
-}while(!isNaN(Number(input)));
-    console.log(input);
+// do {
+//     input = prompt("What is your name?");
+// }while(!isNaN(Number(input)));
+//     console.log(input);
 
-alert(`Welcome  ${input}`);
+// alert(`Welcome  ${input}`);
+
+let fruits = ['Apple', 'Banana'];
+fruits.push('Orange');
+fruits.pop();
+fruits.unshift("Mango");
+fruits.shift()
+
+console.log(fruits)
+
+for (let i = -; i < fruits.length; i++)
+{
+    console.log(fruits[i]);
+}
+for (let item in fruites)
+{
+    console.log(item);
+}
+
+const  person = {
+    name:['Bob','Smith'],
+    age: 32,
+    gender: 'male',
+    interests: ["music", "hiking"],
+}
+console.log(`My name is ${person.name[0]} ${person.name[1]}`)
+console.log(`I am interested in ${interests[0]} and ${interests[1]}`)
+
+for (let props in person)
+{
+    console.log(`My ${prop} is ${person[prop]}`)
+}
